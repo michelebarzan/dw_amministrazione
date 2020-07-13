d0c019a216d00510f212360554bfd94fca26245a
Michele Barzan <michele@servizioglobale.it>
2020-07-13 08:50:05 +0200

    importazione txt ok

--- importaTxt.php
+++ importaTxt.php
@@ -92,6 +92,118 @@
                     $rowString=substr($rowString,0,strlen($rowString)-2);
                     $rigaCheck=strtolower($rowString);
                     $colonnaCheck=strtolower($columns[0]);
+                    //controllo che la riga non sia l'intestazione
+                    if (strpos($rigaCheck, $colonnaCheck) === false) 
+                    {
+                        $rowString=str_replace(chr(34),"'",$rowString);
+                        $rowArray=explode(chr(9),$rowString);
+                        //controllo di non aver piu colonne nella tabella che nella rigab del txt
+                        if(sizeof($columns)>sizeof($rowArray))
+                        {
+                            $columnsL=sizeof($columns);
+                            $rowArrayL=sizeof($rowArray);
+
+                            $diff=$columnsL-$rowArrayL;
+                            for ($x=0; $x < $diff; $x++) 
+                            { 
+                                array_push($rowArray,"NULL");
+                            }
+                        }
+                        //controllo di non aver piu colonne nella riga del txt che nella tabella 
+                        if(sizeof($rowArray)<sizeof($columns))
+                        {
+                            $columnsL=sizeof($columns);
+                            $rowArrayL=sizeof($rowArray);
+
+                            $diff=$rowArrayL-$columnsL;
+                            array_splice($array, count($array) - $diff, $diff);
+                        }
+                        //se il numero di colonnne e uguale
+                        if(sizeof($rowArray)===sizeof($columns))
+                        {        
+                            //controllo i valori dell' array, se sono nulli o altri caratteri li sistemo
+                            for ($y=0; $y < sizeof($rowArray); $y++)
+                            {
+                                $item=$rowArray[$y];
+                                if($item=="" || $item==" " || $item==null || strlen($item)==0 || ord($item)==13)
+                                {
+                                    if(($data_types[$y]=="decimal" || $data_types[$y]=="int" || $data_types[$y]=="real" || $data_types[$y]=="smallint"))
+                                        $rowArray[$y]=0;
+                                    else
+                                        $rowArray[$y]="''";
+                                }
+                            }
+
+                            $valoreColonna0=str_replace("'","",$rowArray[0]);
+                            array_push($columns0file,$valoreColonna0);
+                            
+                            //$queries è un array di array contenenti query, è un array associativo con indice il valore della prima colonna
+                            if(!isset($queries[$valoreColonna0]) || $queries[$valoreColonna0]==null)
+                                $queries[$valoreColonna0]=[];
+                            array_push($queries[$valoreColonna0],"INSERT INTO [$table] ($columns_string) VALUES (".implode(',',$rowArray).")");
+                        }
+                    }
+                    $rowN++;
+                }
+                fclose($file);
+
+                $codiciDaInserire=array_diff($columns0file,$columns0array);
+                $codiciDaInserire=array_unique($codiciDaInserire);
+                
+                foreach($codiciDaInserire as $codice)
+                {
+                    $queries_list=$queries[$codice];
+                    foreach($queries_list as $q2)
+                    {
+                        $r2=sqlsrv_query($conn,$q2);
+                        if($r2==FALSE)
+                        {
+                            array_push($errorMessages,"<b>Tabella: </b>$database.$table<br><b>Query: </b>".$q2."<br>");
+                            $righeNonInserite++;
+                        }
+                        else
+                        {
+                            $righeInserite++;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    $time_elapsed_secs = microtime(true) - $start;
+    $time_elapsed_secs = number_format($time_elapsed_secs,1);
+
+    $arrayResponse["righeInserite"]=$righeInserite;
+    $arrayResponse["righeNonInserite"]=$righeNonInserite;
+    $arrayResponse["errorMessages"]=$errorMessages;
+    $arrayResponse["time_elapsed_secs"]=$time_elapsed_secs;
+
+    echo json_encode($arrayResponse);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    /*while(!feof($file))
+                {
+                    $rowString=fgets($file);
+                    $rowString=substr($rowString,0,strlen($rowString)-2);
+                    $rigaCheck=strtolower($rowString);
+                    $colonnaCheck=strtolower($columns[0]);
                     if (strpos($rigaCheck, $colonnaCheck) === false) 
                     {
                         $rowString=str_replace(chr(34),"'",$rowString);
@@ -158,16 +270,5 @@
                 }
             }
         }
-    }
-
-    $time_elapsed_secs = microtime(true) - $start;
-    $time_elapsed_secs = number_format($time_elapsed_secs,1);
-
-    $arrayResponse["righeInserite"]=$righeInserite;
-    $arrayResponse["righeNonInserite"]=$righeNonInserite;
-    $arrayResponse["errorMessages"]=$errorMessages;
-    $arrayResponse["time_elapsed_secs"]=$time_elapsed_secs;
-
-    echo json_encode($arrayResponse);
-
+    }*/
 ?>
